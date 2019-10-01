da44c9d190799b4d6b8135ecfde9f160d7e218e4
Manuel Stutz <manuel.stutz@students.fhnw.ch>
2019-10-01 15:43:17 +0200

    Initial development Lab02

--- DotNetBay.WPF/App.xaml.cs
+++ DotNetBay.WPF/App.xaml.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using DotNetBay.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using System.Windows;
+using DotNetBay.Core.Execution;
+using DotNetBay.Data.Provider.FileStorage;
 
 namespace DotNetBay.WPF
 {
--- DotNetBay.WPF/MainWindow.xaml.cs
+++ DotNetBay.WPF/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,11 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using DotNetBay.Core;
+using DotNetBay.Core.Execution;
+using DotNetBay.Data.Entity;
+using DotNetBay.Data.Provider.FileStorage;
+using DotNetBay.Interfaces;
 
 namespace DotNetBay.WPF
 {
@@ -20,9 +26,44 @@ namespace DotNetBay.WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        public readonly IMainRepository MainRepository;
+        public readonly IAuctionRunner AuctionRunner;
+        private readonly IAuctionService auctionService;
+
+        public ObservableCollection<Auction> Auctions { get; }
+
         public MainWindow()
         {
             InitializeComponent();
+
+            this.MainRepository = new FileSystemMainRepository("mainRepository.txt");
+            this.AuctionRunner = new AuctionRunner(this.MainRepository);
+            this.AuctionRunner.Start();
+
+            //AddRandomData();
+
+            DataContext = this;
+
+            this.auctionService = new AuctionService(this.MainRepository, new SimpleMemberService(this.MainRepository));
+            this.Auctions = new ObservableCollection<Auction>(this.auctionService.GetAll());
+        }
+
+        void AddRandomData()
+        {
+            var memberService = new SimpleMemberService(this.MainRepository);
+            var service = new AuctionService(this.MainRepository, memberService);
+            if (!service.GetAll().Any())
+            {
+                var me = memberService.GetCurrentMember();
+                service.Save(new Auction
+                {
+                    Title = "My First Auction",
+                    StartDateTimeUtc = DateTime.UtcNow.AddSeconds(10),
+                    EndDateTimeUtc = DateTime.UtcNow.AddDays(14),
+                    StartPrice = 72,
+                    Seller = me
+                });
+            }
         }
     }
 }
